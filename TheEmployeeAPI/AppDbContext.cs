6c9ffb196637d800b9d7b9114ab704f56315675d
Saad Zafar <saadzafar.code@gmail.com>
2024-12-21 23:15:23 +0100

    Stuck, can't proceed non-null error

---
+++ TheEmployeeAPI/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
